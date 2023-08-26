2e8782ca2ded8e59695bb7e33daa201de21ebbd4
Blazer9001 <blazer9003@gmail.com>
2023-08-26 13:00:27 -0400

    update asset paths

--- scripts/hell.js
+++ scripts/hell.js
@@ -1,6 +1,6 @@
 function scream() {
     document.getElementById("inputField").value = "";
 
-    let scream = new Audio("/assets/scream.mp3");
+    let scream = new Audio("/assets/audio/scream.mp3");
     scream.play();
 }
